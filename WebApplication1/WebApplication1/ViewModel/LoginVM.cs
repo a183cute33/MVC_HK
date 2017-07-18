0629762e6d7205cc7c0c37b1afdb3688d99458e3
方鈺茹(Lulu Fang) <1608020@iisigroup.com>
2017-07-18 17:05:56 +0800

    viewMode 建立 login.cshtml 建立 controller add login action

---
+++ WebApplication1/WebApplication1/ViewModel/LoginVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.ViewModel
+{
+    public class LoginVM
+    {
+        public string account { get;set;}
+        public string password { get; set; }
+
+    }
+}
